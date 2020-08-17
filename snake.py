cf0820b8e11a464aae965370c16363c776e46daf
Luke <luke.gilsinger@gmail.com>
2020-08-16 21:53:45 -0400

    Fixed a bug where if you lose you have to restart the program to play again

--- snake.py
+++ snake.py
@@ -69,90 +69,89 @@ def gameLoop(dis, snake_style_pos, clock):
 
         while game_close == True:
             pygame.quit()
-            while True:
-                il.intro(True)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_d:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_w:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_s:
-                    y1_change = snake_block
-                    x1_change = 0
+            game_over = True
+            break
+        if game_over is False:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        x1_change = -snake_block
+                        y1_change = 0
+                    elif event.key == pygame.K_d:
+                        x1_change = snake_block
+                        y1_change = 0
+                    elif event.key == pygame.K_w:
+                        y1_change = -snake_block
+                        x1_change = 0
+                    elif event.key == pygame.K_s:
+                        y1_change = snake_block
+                        x1_change = 0
 
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-            game_close = True
-        x1 += x1_change
-        y1 += y1_change
-        dis.fill(blue)
-        for x in range(100):
-            try:
-                foodx = foodcords['x'][x]
-                foody = foodcords['y'][x]
-            except KeyError:
-                pass
-            food(green, foodx, foody, snake_block)
-        snake_Head = []
-        snake_Head.append(x1)
-        snake_Head.append(y1)
-        style = snake_style[snake_style_pos]
-        style_len = len(snake_style) - 1
-        if style_len == snake_style_pos:
-            snake_style_pos = 0
-        elif style_len == 0:
-            snake_style_pos = 1
-        else:
-            snake_style_pos = snake_style_pos + 1
-        snake_Head.append(style)
-        snake_List.append(snake_Head)
-        if len(snake_List) > Length_of_snake:
-            del snake_List[0]
-        for x in snake_List[:-1]:
-            if x == snake_Head:
+            if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
                 game_close = True
-
-        our_snake(snake_block, snake_List)
-        Your_score(Length_of_snake - 1)
-        pygame.display.update()
-        for x in range(100):
-            try:
-                cords_y = foodcords['y'][x]
-                cords_x = foodcords['x'][x]
-            except KeyError:
-                pass
-            if x1 == cords_x and y1 == cords_y:
+            x1 += x1_change
+            y1 += y1_change
+            dis.fill(blue)
+            for x in range(100):
                 try:
-                    del foodcords['y'][x]
-                    del foodcords['x'][x]
+                    foodx = foodcords['x'][x]
+                    foody = foodcords['y'][x]
                 except KeyError:
                     pass
-                Length_of_snake += 1
-                high_score_file = open('high_score.pyv', 'r+')
-                high_score = int(high_score_file.read().replace('\x00', ''))
-                if Length_of_snake - 1 > high_score:
-                    high_score = high_score + 1
-                    high_score_file.truncate(0)
-                    high_score_file.write(str(high_score))
-                high_score_file.close()
-        clock.tick(snake_speed)
+                food(green, foodx, foody, snake_block)
+            snake_Head = []
+            snake_Head.append(x1)
+            snake_Head.append(y1)
+            style = snake_style[snake_style_pos]
+            style_len = len(snake_style) - 1
+            if style_len == snake_style_pos:
+                snake_style_pos = 0
+            elif style_len == 0:
+                snake_style_pos = 1
+            else:
+                snake_style_pos = snake_style_pos + 1
+            snake_Head.append(style)
+            snake_List.append(snake_Head)
+            if len(snake_List) > Length_of_snake:
+                del snake_List[0]
+            for x in snake_List[:-1]:
+                if x == snake_Head:
+                    game_close = True
 
-    pygame.quit()
-    quit()
+            our_snake(snake_block, snake_List)
+            Your_score(Length_of_snake - 1)
+            pygame.display.update()
+            for x in range(100):
+                try:
+                    cords_y = foodcords['y'][x]
+                    cords_x = foodcords['x'][x]
+                except KeyError:
+                    pass
+                if x1 == cords_x and y1 == cords_y:
+                    try:
+                        del foodcords['y'][x]
+                        del foodcords['x'][x]
+                    except KeyError:
+                        pass
+                    Length_of_snake += 1
+                    high_score_file = open('high_score.pyv', 'r+')
+                    high_score = int(high_score_file.read().replace('\x00', ''))
+                    if Length_of_snake - 1 > high_score:
+                        high_score = high_score + 1
+                        high_score_file.truncate(0)
+                        high_score_file.write(str(high_score))
+                    high_score_file.close()
+            clock.tick(snake_speed)
 
 
 il.intro(False)
-pygame.init()
-display = pygame.display
-dis = display.set_mode((0, 0), pygame.FULLSCREEN)
-pygame.display.set_caption('Snake Game by Ultraprogamer and Lukeg007')
-clock = pygame.time.Clock()
-gameLoop(display.set_mode((0, 0), pygame.FULLSCREEN), snake_style_pos, clock)
+while True:
+    pygame.init()
+    display = pygame.display
+    dis = display.set_mode((0, 0), pygame.FULLSCREEN)
+    clock = pygame.time.Clock()
+    gameLoop(display.set_mode((0, 0), pygame.FULLSCREEN), snake_style_pos, clock)
+    il.intro(True)
